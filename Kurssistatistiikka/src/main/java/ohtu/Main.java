11de36dcc2e0ac927b644479149138c41da6d41a
Vertti Viitanen <vertti.viitanen@gmail.com>
2018-11-17 23:52:55 +0200

    - Exercise 1

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,36 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // ÄLÄ laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        
+        int hours = 0;
+        int exercises = 0;
+        System.out.println("opiskelijanumero " + studentNr + "\n");
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            hours += submission.getHours();
+            exercises += submission.getExerciseAmount();
+        }
+        
+        String formatHour = (hours == 1) ? " tunti" : " tuntia";
+        String formatExercise = (exercises == 1) ? " tehtävä," : " tehtävää,";
+        System.out.println("\n yhteensä: " + exercises + formatExercise + hours + formatHour);
     }
 }
--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -2,18 +2,28 @@ package ohtu;
 
 public class Submission {
     private int week;
+    private int hours;
+    private int[] exercises;
+    private String course;
 
-    public void setWeek(int week) {
-        this.week = week;
+    public int getHours(){
+        return hours;
     }
-
-    public int getWeek() {
-        return week;
+    
+    public int getExerciseAmount(){
+        return (exercises == null) ? 0 : exercises.length;
     }
-
+    
     @Override
     public String toString() {
-        return ""+week;
+        String format = (hours == 1) ? " tunti" : " tuntia";
+        String exerciseString = "";
+        for (int i = 0; i < exercises.length; ++i){
+            exerciseString += " " + exercises[i];
+            if (i < exercises.length - 1)
+                exerciseString += ",";
+        }
+        return course + ", viikko " + week + " tehtyjä tehtäviä yhteensä " + exercises.length + ", aikaa kului " + hours + format + ", tehdyt tehtävät:" + exerciseString;
     }
     
 }
